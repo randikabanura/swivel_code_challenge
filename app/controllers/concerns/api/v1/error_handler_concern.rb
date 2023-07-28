69b055c31a0a350048dd6789fe5fffdffd9fa873
Banura Randika <randika.banura@gmail.com>
2023-07-28 11:07:26 +0530

    Base controller add

---
+++ app/controllers/concerns/api/v1/error_handler_concern.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    module ErrorHandlerConcern
+      include Api::V1::ResponseHandlerConcern
+
+      def self.included(base)
+        base.class_eval do
+          rescue_from StandardError do |e|
+            respond(500, e.to_s)
+          end
+        end
+      end
+
+      private
+
+      def respond(status, message)
+        render json: exception_response(message), status: status
+      end
+    end
+  end
+end
